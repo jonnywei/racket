c5117284d95c917401565ceeba60ff7c83d9be4c
Jonny Wei <weijianjun7100@gmail.com>
2013-07-11 08:13:07 +0800

    add fib

--- serve-thread.rkt
+++ serve-thread.rkt
@@ -1,14 +1,16 @@
 #lang racket
 (define (serve port-no)
-  (define listener (tcp-listen port-no 5 #t))
-  (define (loop)
-    (accept-and-handle listener)
-    (loop))
+  (define main-cust (make-custodian))
+  (parameterize ([current-custodian main-cust])
+    (define listener (tcp-listen port-no 5 #t))
+    (define (loop)
+      (accept-and-handle listener)
+      (loop))
   ;define线程，loop run in its own thread
-  (define t (thread loop))
+  (thread loop))
   (lambda ()
-    (kill-thread t)
-    (tcp-close listener)))
+    (custodian-shutdown-all main-cust)))
+    
   
 
 (define (accept-and-handle listener)
@@ -26,15 +28,39 @@
             (sleep 10)
             (custodian-shutdown-all cust))))
  
-
+(require xml net/url)
 (define (handle in out)
-  ;discard the request header 
-  (regexp-match #rx"(\r\n|^)\r\n" in)
-  ; sleep random seconds
-  ;(sleep 33)
-  ;send reply
-  (display "HTTP/1.0 200 OK\r\n" out)
-  (display "Server:racket\r\nContent-Type:text/html\r\n\r\n" out)
-  (display "<html><body>Hello,racket!</body></html>" out))
+  (define req 
+    ;match the fire line to extract request
+    (regexp-match #rx"^GET (.+) HTTP/[0-9]+\\.[0-9]+"
+                  (read-line in)))
+  (when req
+    ;discard the request header 
+    (regexp-match #rx"(\r\n|^)\r\n" in)
+    (let ([xexpr (dispatch (list-ref req 1))])
+    ; sleep random seconds
+    ;(sleep 33)
+    ;send reply
+      (display "HTTP/1.0 200 OK\r\n" out)
+      (display "Server:racket\r\nContent-Type:text/html\r\n\r\n" out)
+      (display (xexpr->string xexpr) out))))
 
+;define dispatch
+(define (dispatch str-path)
+  ;parse the request as url
+  (define url (string->url str-path))
+  ;extarct the path param
+  (define path (map path/param-path (url-path url)))
+  ;find a handler based on the path's first element
+  (define h (hash-ref dispatch-table (car path) #f))
+  (if h
+      (h (url-query url))
+      ;no handler found:
+      `(html (head (title "Error"))
+             (body 
+              (font ((color "red"))
+                    "Unknown page: "
+                    ,str-path)))))
+
+(define dispatch-table (make-hash))
 
